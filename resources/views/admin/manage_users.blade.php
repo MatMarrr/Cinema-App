27fe378cc135b612f10b3cfb731390b208a235e4
MatMarrr <mateuszrmarek@gmail.com>
2023-08-09 23:18:04 +0200

    Added session->get in manage users

--- resources/views/admin/manage_users.blade.php
+++ resources/views/admin/manage_users.blade.php
@@ -6,21 +6,21 @@
 
 @section('content')
 
-    @if(session()->has('showDeleteUserModal'))
+    @if(session()->has('showDeleteUserModal') && session()->has('user_name'))
         <div class="fixed z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
             <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
                 <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
                 <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
                 <div
-                    class="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
+                        class="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
                     <div>
                         <div class="mt-3 text-center sm:mt-5">
                             <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-title">Are you sure that
-                                you want to delete: {{session('user_name')}}</h3>
+                                you want to delete: {{session()->get('user_name')}}</h3>
                         </div>
                     </div>
                     <div class="mt-5 sm:mt-6 sm:grid sm:grid-cols-2 sm:gap-3 sm:grid-flow-row-dense">
-                        <form action="{{ route('admin.delete.user', ['user_id' => session('user_id')]) }}" method="POST" class="w-full sm:col-start-2">
+                        <form action="{{ route('admin.delete.user', ['user_id' => session()->get('user_id')]) }}" method="POST" class="w-full sm:col-start-2">
                             @csrf
                             @method('DELETE')
                             <button type="submit"
